24f2b95e27e508d485a3a5769863d132b2954445
snyder-io <tushar.whitecollar@gmail.com>
2024-11-24 20:47:16 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,49 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+import User from "../models/user.model.js";
+import apiResponse from "../utils/apiResponse.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Register User",
-        status: "OK"
-    });
-})
-export const loginUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Login User",
-        status: "OK"
-    });
+    // Get user data from request body
+    // Validation - not empty
+    // Check if user already exists: username, email
+    //Create user object - create entry in database
+    // Remove password and refresh token from response
+    // Check for user creation
+    //return response
+    const { userName, email, firstName, lastName, password, } = req.body;
+
+    if (!userName || !email || !firstName || !lastName || !password) {
+        throw new apiError(400, "All fields are required");
+    }
+    const existedUser = User.findOne({
+        $or: [
+            { userName },
+            { email }
+        ]
+    })
+    if (existedUser) {
+        throw new apiError(409, "User already exists");
+    }
+    const user = await User.create({
+        userName: userName.toLowerCase(),
+        email,
+        firstName,
+        lastName,
+        password
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken "
+    )
+    if (!createdUser) {
+        throw new apiError(500, "User not created")
+    }
+    return res.status(201).json(
+        new apiResponse(200, createdUser, "User created successfully")
+    )
+
+
+
+
 })
